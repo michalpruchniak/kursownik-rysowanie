c75bc2fdaabe7cd0236d4135d45b37bee8b329d5
Michal Pruchniak <mpruchniak@gmail.com>
2017-03-19 13:52:31 +0100

    lesson 2

--- js/main.js
+++ js/main.js
@@ -4,4 +4,14 @@ my_canvas.height = 500;
 document.body.appendChild(my_canvas);
 
 var ctx = my_canvas.getContext('2d');
-ctx.fillRect(100, 150, 250, 50);
+// ctx.fillRect(20, 20, 100, 100);
+ctx.strokeRect(140, 20, 50, 50);
+ctx.clearRect(60, 30, 100, 50);
+
+ctx.beginPath();
+ctx.moveTo(30, 40);
+ctx.lineTo(100, 140);
+ctx.lineTo(60, 250);
+ctx.lineTo(20, 150);
+ctx.closePath();
+ctx.stroke();
